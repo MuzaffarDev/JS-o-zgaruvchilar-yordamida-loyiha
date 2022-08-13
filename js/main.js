08e1a2b53da2d641fea64841b0f8d4e58010b634
MuzaffaR <abdurahmonovmuzaffar56@gmail.com>
2022-08-13 22:39:45 +0500

    js o'zgaruvchilar prompt yordamida loyiha

---
+++ js/main.js
@@ -0,0 +1,29 @@
+alert(
+  "Assalomu alleykum siz ushbu so'rovnomani to'ldirganingizdan keyin ushbu havolaga o'ta olasiz!!!"
+);
+
+
+let name = prompt("Ism Sharifingiz?"); //Ismingiz sharifingiz nima?
+alert(`Salom ${name}!`); //Salom name
+
+let age = prompt(`'${name} yoshingiz nechida?,100'`); //Nechi yosh?
+
+let life = (`Hurmatli ${name} Siz ${age} yoshgacha qayerlarda o'qigansiz?`); //name siz age yosh
+
+let study = prompt(`${name} ma'lumotingiz qanaqa?`); //Malumotingiz
+alert(`"${study}"juda yaxshi ;-)`);
+
+
+let parol = prompt(`Xavsizlik maqsadida parol kiriting`);
+alert(`parol saqlandi`);
+
+let email = prompt(`emailingizni kiriting`);
+alert(`email saqlandi`);
+
+
+
+let job = prompt(`${name} kasbingiz nima?`);
+alert(`${job} ajoyib`);
+alert(
+  `ushbu havolamizdagi ma'lumotlar sizga manfaatli bo'ladi degan umiddamiza!`
+);
